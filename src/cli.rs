960ee9719dcbccede75b7aca04b67f59b14e5f2c
Ayush Modi <ayushmodi2003@gmail.com>
2025-11-14 11:13:12 +0530

    add version

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,7 @@
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[command(name = "eth", about = "ethereum made simple")]
+#[command(name = "eth", version, about = "ethereum made simple")]
 pub struct Cli {
     #[command(subcommand)]
     pub command: Commands,
